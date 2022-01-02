6aab140c109ef174fb67e7753550189aab42bd42
Florent Clarret <florent.clarret@gmail.com>
2022-01-02 22:34:28 +0100

    Add docker aliases

--- dot_oh-my-zsh/custom/docker.zsh
+++ dot_oh-my-zsh/custom/docker.zsh
@@ -1,3 +1,12 @@
 #!/bin/zsh
 
 alias dk="docker"
+
+# Get container process
+alias dkps="docker ps"
+# Get process included stop container
+alias dkpsa="docker ps -a"
+# Get images
+alias dki="docker images"
+# Stop and Remove all containers
+alias dkrmf='docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q)'
